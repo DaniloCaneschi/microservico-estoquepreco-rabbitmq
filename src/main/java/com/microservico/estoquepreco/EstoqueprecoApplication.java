14c450b2484a62571da88b011382f0b2b243e4c8
Danilo Caneschi <caneschi.danilo@gmail.com>
2021-05-26 21:18:26 -0300

    Instalação do Rabbitmq e configuração da conexão com rabbitmq.

---
+++ src/main/java/com/microservico/estoquepreco/EstoqueprecoApplication.java
@@ -0,0 +1,12 @@
+package com.microservico.estoquepreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueprecoApplication {
+
+  public static void main(String[] args){
+    SpringApplication.run(EstoqueprecoApplication.class, args);
+  }
+}
