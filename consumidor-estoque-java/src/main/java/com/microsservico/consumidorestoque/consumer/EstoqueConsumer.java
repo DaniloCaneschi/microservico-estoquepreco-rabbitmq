1dfde49d4ce63238fbcb8e5d6229cc98283ec490
Danilo Caneschi <51996690+DaniloCaneschi@users.noreply.github.com>
2021-08-23 08:03:47 -0300

    Conversão da mensagem em Json para EstoqueDto

--- consumidor-estoque-java/src/main/java/com/microsservico/consumidorestoque/consumer/EstoqueConsumer.java
+++ consumidor-estoque-java/src/main/java/com/microsservico/consumidorestoque/consumer/EstoqueConsumer.java
@@ -1,5 +1,7 @@
 package com.microsservico.consumidorestoque.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import constantes.RabbitmqConstantes;
 import dto.EstoqueDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Component;
 public class EstoqueConsumer {
 
   @RabbitListener(queues = RabbitmqConstantes.FILA_ESTOQUE)
-  private void consumidor(EstoqueDto estoqueDto){
+  private void consumidor(String mensagem) throws JsonProcessingException {
+    EstoqueDto estoqueDto = new ObjectMapper().readValue(mensagem, EstoqueDto.class);
+
     System.out.println(estoqueDto.codigoproduto);
     System.out.println(estoqueDto.quantidade);
     System.out.println("------------------------------------");
